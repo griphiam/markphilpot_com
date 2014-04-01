8ac306b0930a8f7f49956cf68de3aad9dc76282f
Mark Philpot <mark.philpot@gmail.com>
2014-03-31 21:06:26 -0700

    More tweaks with submodules

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,5 +27,8 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+PLUGIN_PATH = 'plugins'
+PLUGINS = []
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
