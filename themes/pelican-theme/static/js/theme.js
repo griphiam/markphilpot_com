1811214165138e2232e1b2ac0238dea0c2400552
Mark Philpot <mark.philpot@gmail.com>
2018-09-29 09:08:54 -0700

    Working switch

--- themes/pelican-theme/static/js/theme.js
+++ themes/pelican-theme/static/js/theme.js
@@ -1,9 +1,25 @@
-function setTheme(siteUrl){
-	const theme = readCookie('theme') || 'light';
+function setTheme(siteUrl, theme){
+	// var lnk = document.createElement('link');
+	// lnk.id='theme-css'
+	// lnk.type='text/css';
+	// lnk.href=`${siteUrl}/theme/css/${theme}.css`;
+	// lnk.rel='stylesheet';
+	// document.getElementsByTagName('head')[0].appendChild(lnk);
+	// createCookie('theme', theme);
+	const linkTags = document.getElementsByTagName("link");
+	for(var i = 0; i<linkTags.length; i++){
+		if(linkTags[i] && 
+			linkTags[i].rel.indexOf("stylesheet") !== -1 && 
+			linkTags[i].title ){
+			linkTags[i].disabled = true;
+		}
+		if(linkTags[i] && linkTags[i].title === theme) {
+			linkTags[i].disabled = false;
+		}
+	}
+	createCookie('theme', theme);
+}
 
-	var lnk = document.createElement('link');
-	lnk.type='text/css';
-	lnk.href=`${siteUrl}/theme/css/${theme}.css`;
-	lnk.rel='stylesheet';
-	document.getElementsByTagName('head')[0].appendChild(lnk);
+function changeTheme(siteUrl, theme) {
+	document.getElementById('theme-css').href = `${siteUrl}/theme/css/${theme}.css`;
 }
